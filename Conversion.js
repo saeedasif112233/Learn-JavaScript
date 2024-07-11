19fd15c73beb13c19e35c0164c19b9fb823bbb59
Saeed Asif <162897142+saeedasif112233@users.noreply.github.com>
2024-07-11 11:53:51 +0500

    Adding Convesion file

---
+++ Conversion.js
@@ -0,0 +1,15 @@
+let score = "299"
+console.log(typeof (score));
+
+let scoreIntoNum = Number(score)
+
+console.log(typeof (scoreIntoNum));
+
+// Conversion to Number
+
+// '30' => 30
+// '30abc'=> NaN
+// true => 1 ,0 => false
+//null => 0
+//undefined => NaN
+
---
+++ Intro/Variable.js
@@ -0,0 +1,14 @@
+const acountId = 1442121
+let accountEmail = 'saeedasif576881@gmail.com'
+let passward = "123@123"
+accountCity = 'Lahore'
+
+// acountId = 4; // Not Allowed in cost
+
+console.log(acountId);
+
+console.log('====================================');
+console.log([accountEmail,passward,accountCity]);
+console.log('====================================');
+
+
